55117fe54e4be27cb34dcdbd04cd7c07a9630131
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-18 16:41:36 +0600

    seventh commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { RouterProvider } from 'react-router-dom'
 import routers from './routers/routers.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={routers}></RouterProvider>
-  </StrictMode>,
+    <ToastContainer
+      position="top-center"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition:Bounce
+    ></ToastContainer>
+  </StrictMode >,
 )
--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,25 +1,100 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const DetailsPage = () => {
 
     const clothe = useLoaderData()
     const { id, image, division, description, title, contactInfo, status } = clothe
 
+    const handleDonationForm = (e) => {
+        e.preventDefault()
+        const form = new FormData(e.target)
+        const quantity = form.get('quantity')
+        const itemType = form.get('itemType')
+        const pickupLocation = form.get('pickupLocation')
+        const additionalNotes = form.get('additionalNotes')
+        e.target.reset()
+        toast.success('Thank you ! We will reach your destination soon')
+    }
+
     return (
-        <div className="flex justify-center mt-10">
-            <div className="border-2 border-blue-300 rounded-lg p-4 space-y-4">
-                <figure>
-                    <img className="rounded-xl h-96"
-                        src={image}
-                        alt="Shoes" />
-                </figure>
-                <div className="space-y-4">
-                    <h2 className="card-title">{title}</h2>
-                    <p className="text-gray-600">{description}</p>
-                    <p className="text-gray-600"><span className="text-black font-semibold">Status: </span>{status}</p>
-                    <p className="text-gray-600"><span className="text-black font-semibold">Contact: </span>{contactInfo}</p>
-                    <p><span className="font-semibold text-black">Division: </span>{division}</p>
+        <div className="max-w-2xl mx-auto">
+            <div className="flex justify-center mt-10">
+                <div className="border-2 border-blue-300 rounded-lg p-4 space-y-4">
+                    <figure>
+                        <img className="rounded-xl h-96"
+                            src={image}
+                            alt="Shoes" />
+                    </figure>
+                    <div className="space-y-4">
+                        <h2 className="card-title">{title}</h2>
+                        <p className="text-gray-600">{description}</p>
+                        <p className="text-gray-600"><span className="text-black font-semibold">Status: </span>{status}</p>
+                        <p className="text-gray-600"><span className="text-black font-semibold">Contact: </span>{contactInfo}</p>
+                        <p><span className="font-semibold text-black">Division: </span>{division}</p>
+                    </div>
                 </div>
+            </div>
+            <div className="border-2 border-blue-300 rounded-lg p-4 mt-10">
+                <form onSubmit={handleDonationForm} className="p-4 space-y-4 rounded-lg">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Quantity</span>
+                        </label>
+                        <input
+                            type="number"
+                            name="quantity"
+                            placeholder="Enter quantity (e.g.,2 jackets, 3 blankets)"
+                            className="input input-bordered"
+                            required
+                        />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Item Type</span>
+                        </label>
+                        <select
+                            name="itemType"
+                            className="select select-bordered"
+                            required
+                        >
+                            <option value="" disabled>
+                                Select item type
+                            </option>
+                            <option value="Blanket">Blanket</option>
+                            <option value="Jacket">Jacket</option>
+                            <option value="Sweater">Sweater</option>
+                        </select>
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Pickup Location</span>
+                        </label>
+                        <input
+                            type="text"
+                            name="pickupLocation"
+                            placeholder="Enter pickup location"
+                            className="input input-bordered"
+                            required
+                        />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Additional Notes</span>
+                        </label>
+                        <textarea
+                            name="additionalNotes"
+                            placeholder="Enter any additional notes (optional)"
+                            className="textarea textarea-bordered"
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="btn bg-[#F59E0B] w-full text-white">
+                        Submission
+                    </button>
+                </form>
             </div>
         </div>
     );
