2748940553be84103fc10218371bde602a10a09a
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-18 09:18:40 +0600

    footer commit

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <footer className="mt-96">
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };
